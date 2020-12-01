77d0c60d38160237a79a9ed39162b7dc5822c78d
jazielloureiro <jazielloureiro17@gmail.com>
2020-12-01 08:11:57 -0300

    Add scripts

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.getElementById("password").value = "Hello!";
+
+function updateTextValue(value){
+	document.getElementById("textInput").value = value;
+}
+
+function updateSliderValue(value){
+	document.getElementById("slider").value = value;
+}
