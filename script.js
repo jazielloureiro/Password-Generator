7695af962bccb1cddd7c0e233f1520da1082213c
jazielloureiro <jazielloureiro17@gmail.com>
2021-07-13 16:21:25 -0300

    Remove inline javascript

--- script.js
+++ script.js
@@ -1,58 +1,73 @@
-function copyToClipboard(){
+const password = document.getElementById('password');
+const copyButton = document.getElementById('copy-button');
+const generateButton = document.getElementById('generate-button');
+
+const slider = document.getElementById('slider');
+const textInput = document.getElementById('text-input');
+
+const lowercaseCheckbox = document.getElementById('lowercase');
+const uppercaseCheckbox = document.getElementById('uppercase');
+const numbersCheckbox = document.getElementById('numbers');
+const symbolsCheckbox = document.getElementById('symbols');
+
+function generatePassword() {
+	let characters = '', newPassword = '';
+
+	if(lowercaseCheckbox.checked)
+		characters = characters.concat('abcdefghijklmnopqrstuvwxyz');
+
+	if(uppercaseCheckbox.checked)
+		characters = characters.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+	if(numbersCheckbox.checked)
+		characters = characters.concat('0123456789');
+
+	if(symbolsCheckbox.checked)
+		characters = characters.concat('!\'#$%&"()*+,-./:;<=>?@[\\]^_`{|}~');
+
+	if(characters !== '') {
+		for(let i = 0; i < slider.value; i++) {
+			let index = Math.floor(Math.random() * characters.length);
+
+			newPassword = newPassword.concat(characters.charAt(index));
+		}
+	}
+
+	password.value = newPassword;
+}
+
+generateButton.addEventListener('click', generatePassword);
+lowercaseCheckbox.addEventListener('change', generatePassword);
+uppercaseCheckbox.addEventListener('change', generatePassword);
+numbersCheckbox.addEventListener('change', generatePassword);
+symbolsCheckbox.addEventListener('change', generatePassword);
+
+copyButton.addEventListener('click', () => {
 	const minRange = 0, maxRange = 99999;
-	let password = document.getElementById("password");
 
 	password.select();
 	password.setSelectionRange(minRange, maxRange);
 
-	document.execCommand("copy");
-}
+	document.execCommand('copy');
+});
 
-function generatePassword(){
-	const passwordLength = document.getElementById("slider").value;
-	let characters = "", password = "";
-
-
-	if(document.getElementById("lowercase").checked)
-		characters = characters.concat("abcdefghijklmnopqrstuvwxyz");
-
-	if(document.getElementById("uppercase").checked)
-		characters = characters.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-	if(document.getElementById("numbers").checked)
-		characters = characters.concat("0123456789");
-
-	if(document.getElementById("symbols").checked)
-		characters = characters.concat("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-
-	if(characters !== ""){
-		for(let i = 0; i < passwordLength; i++){
-			let index = Math.floor(Math.random() * characters.length);
-
-			password = password.concat(characters.charAt(index));
-		}
-	}
-
-	document.getElementById("password").value = password;
-}
-
-function updateTextValue(value){
-	document.getElementById("text-input").value = value;
+slider.addEventListener('change', () => {
+	textInput.value = slider.value;
 
 	generatePassword();
-}
+});
 
-function updateSliderValue(value){
+textInput.addEventListener('change', () => {
 	const minValue = 3, maxValue = 32;
 
-	if(value < minValue)
-		value = minValue;
-	else if(value > maxValue)
-		value = maxValue;
+	if(textInput.value < minValue)
+		textInput.value = minValue;
+	else if(textInput.value > maxValue)
+		textInput.value = maxValue;
 
-	document.getElementById("slider").value = value;
+	slider.value = textInput.value;
 
-	updateTextValue(value);
-}
+	generatePassword();
+});
 
 generatePassword();
